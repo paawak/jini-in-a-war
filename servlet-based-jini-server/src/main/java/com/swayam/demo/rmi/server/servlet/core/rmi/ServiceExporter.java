7698218f1809557cfd052a68b9df44d87056c613
Palash Ray <paawak@gmail.com>
2015-05-19 00:07:31 -0400

    correct url

--- servlet-based-jini-server/src/main/java/com/swayam/demo/rmi/server/servlet/core/rmi/ServiceExporter.java
+++ servlet-based-jini-server/src/main/java/com/swayam/demo/rmi/server/servlet/core/rmi/ServiceExporter.java
@@ -22,9 +22,10 @@ import com.swayam.demo.rmi.server.servlet.core.jini.servlet.ServletBasedServerEn
 
 public class ServiceExporter implements InitializingBean {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ServiceExporter.class);
+    private static final Logger LOG = LoggerFactory
+	    .getLogger(ServiceExporter.class);
 
-    private static final String SERVER_URL = "http://localhost:8080/RmiServer";
+    private static final String SERVER_URL = "http://localhost:8080/ServletBasedJiniServer";
 
     public static final String REMOTE_METHOD_INVOCATION_URI = "/INVOKE_REMOTE_METHOD";
 
@@ -34,50 +35,57 @@ public class ServiceExporter implements InitializingBean {
     private String serviceName;
 
     public DiscoveryManagement getDiscoveryManager() {
-        return discoveryManager;
+	return discoveryManager;
     }
 
     public void setDiscoveryManager(DiscoveryManagement discoveryManager) {
-        this.discoveryManager = discoveryManager;
+	this.discoveryManager = discoveryManager;
     }
 
     public LeaseRenewalManager getLeaseRenewalManager() {
-        return leaseRenewalManager;
+	return leaseRenewalManager;
     }
 
     public void setLeaseRenewalManager(LeaseRenewalManager leaseRenewalManager) {
-        this.leaseRenewalManager = leaseRenewalManager;
+	this.leaseRenewalManager = leaseRenewalManager;
     }
 
     public Remote getService() {
-        return service;
+	return service;
     }
 
     public void setService(Remote service) {
-        this.service = service;
+	this.service = service;
     }
 
     public String getServiceName() {
-        return serviceName;
+	return serviceName;
     }
 
     public void setServiceName(String serviceName) {
-        this.serviceName = serviceName;
+	this.serviceName = serviceName;
     }
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        Exporter exporter = getExporter();
-        Remote exportedService = exporter.export(service);
-        Uuid uuid = UuidFactory.generate();
-        ServiceID serviceID = new ServiceID(uuid.getMostSignificantBits(), uuid.getLeastSignificantBits());
-        @SuppressWarnings("unused")
-        JoinManager joinManager = new JoinManager(exportedService, new Entry[] { new Name(serviceName) }, serviceID, discoveryManager, leaseRenewalManager);
-        LOG.info("----------------------- Service: {} exported with serviceID: {}", serviceName, serviceID);
+	Exporter exporter = getExporter();
+	Remote exportedService = exporter.export(service);
+	Uuid uuid = UuidFactory.generate();
+	ServiceID serviceID = new ServiceID(uuid.getMostSignificantBits(),
+		uuid.getLeastSignificantBits());
+	@SuppressWarnings("unused")
+	JoinManager joinManager = new JoinManager(exportedService,
+		new Entry[] { new Name(serviceName) }, serviceID,
+		discoveryManager, leaseRenewalManager);
+	LOG.info(
+		"----------------------- Service: {} exported with serviceID: {}",
+		serviceName, serviceID);
     }
 
     private Exporter getExporter() {
-        return new BasicJeriExporter(new ServletBasedServerEndpoint(), new ServletBasedILFactory(SERVER_URL + REMOTE_METHOD_INVOCATION_URI));
+	return new BasicJeriExporter(new ServletBasedServerEndpoint(),
+		new ServletBasedILFactory(SERVER_URL
+			+ REMOTE_METHOD_INVOCATION_URI));
     }
 
 }
